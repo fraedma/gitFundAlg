701225260f09088bc8eefc7fb5c260b1e205a157
fraedma <fraedma@gmail.com>
2021-02-14 16:59:33 -0500

    servidor básico
    creación servidor básico

--- gitFundAlgoritmia/server.js
+++ gitFundAlgoritmia/server.js
@@ -1,11 +1,14 @@
-var express = require('express');
-var app = express();
-const bodyParser = require("body-parser");
+const http = require('http');
 
-app.use(bodyParser.json());
+const hostname = '127.0.0.1';
+const port = 3000;
 
-//app.use(express.static(__dirname + '/public')); //__dir and not _dir
-//var port = 3000; // you can use any port
-app.listen(3000, () => {
-    console.log("Server on port http://localhost:3000");
-  });
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hola Mundo');
+});
+
+server.listen(port, hostname, () => {
+  console.log('El servidor se está ejecutando en http://${hostname}:${port}/');
+});
